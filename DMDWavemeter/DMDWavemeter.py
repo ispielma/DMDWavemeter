6d46ef33a98788097b48c55e33cc20274cc853b1
spielman <spielman@umd.edu>
2020-03-11 11:23:02 -0400

    Implemented all hardware functions!

--- DMDWavemeter/DMDWavemeter.py
+++ DMDWavemeter/DMDWavemeter.py
@@ -139,10 +139,10 @@ class LightCrafterWorker():
     
     
     
-    def program_manual(self, values):
-        for region, value in values.items():
-            data = value
-            data = base64.b64decode(data)
+    def program_manual(self, data):
+        
+        data = base64.b64decode(data)
+        
         # Replace empty data with the black picture
         if not data:
             data = BLANK_BMP
@@ -159,6 +159,156 @@ class LightCrafterWorker():
     def shutdown(self):
         self.sock.close()
 
+def _monkeypatch_imaqdispose():
+    """Monkeypatch a fix to a memory leak bug in pynivision. The pynivision project is
+    no longer active, so we can't contribute this fix upstream. In the long run,
+    hopefully someone (perhaps us) forks it so that bugs can be addressed in the
+    normal way"""
+
+    import nivision.core
+    import ctypes
+
+    _imaqDispose = nivision.core._imaqDispose
+
+    def imaqDispose(obj):
+        if getattr(obj, "_contents", None) is not None:
+            _imaqDispose(ctypes.byref(obj._contents))
+            obj._contents = None
+        if getattr(obj, "value", None) is not None:
+            _imaqDispose(obj)
+            obj.value = None
+        # This is the bugfix: pointers as raw ints were not being disposed:
+        if isinstance(obj, int):
+            _imaqDispose(obj)
+
+    nivision.core.imaqDispose = nv.imaqDispose = imaqDispose
+
+import nivision as nv
+_monkeypatch_imaqdispose()
+
+class IMAQdx_Camera(object):
+    def __init__(self, serial_number):
+
+
+        # Find the camera:
+        print("Finding camera...")
+        for cam in nv.IMAQdxEnumerateCameras(True):
+            if serial_number == (cam.SerialNumberHi << 32) + cam.SerialNumberLo:
+                self.camera = cam
+                break
+        else:
+            msg = f"No connected camera with serial number {serial_number:X} found"
+            raise Exception(msg)
+        # Connect to the camera:
+        print("Connecting to camera...")
+        self.imaqdx = nv.IMAQdxOpenCamera(
+            self.camera.InterfaceName, nv.IMAQdxCameraControlModeController
+        )
+        # Keep an img attribute so we don't have to create it every time
+        self.img = nv.imaqCreateImage(nv.IMAQ_IMAGE_U16)
+        self._abort_acquisition = False
+
+    def set_attributes(self, attr_dict):
+        for k, v in attr_dict.items():
+            self.set_attribute(k, v)
+
+    def set_attribute(self, name, value):
+        """Set the value of the attribute of the given name to the given value"""
+        _value = value  # Keep the original for the sake of the error message
+        if isinstance(_value, str):
+            _value = _value.encode('utf8')
+        try:
+            nv.IMAQdxSetAttribute(self.imaqdx, name.encode('utf8'), _value)
+        except Exception as e:
+            # Add some info to the exception:
+            msg = f"failed to set attribute {name} to {value}"
+            raise Exception(msg) from e
+
+    def get_attribute_names(self, visibility_level, writeable_only=True):
+        """Return a list of all attribute names of readable attributes, for the given
+        visibility level. Optionally return only writeable attributes"""
+        visibilities = {
+            'simple': nv.IMAQdxAttributeVisibilitySimple,
+            'intermediate': nv.IMAQdxAttributeVisibilityIntermediate,
+            'advanced': nv.IMAQdxAttributeVisibilityAdvanced,
+        }
+        visibility_level = visibilities[visibility_level.lower()]
+        attributes = []
+        for a in nv.IMAQdxEnumerateAttributes2(self.imaqdx, b'', visibility_level):
+            if writeable_only and not a.Writable:
+                continue
+            if not a.Readable:
+                continue
+            attributes.append(a.Name.decode('utf8'))
+        return sorted(attributes)
+
+    def get_attribute(self, name):
+        """Return current value of attribute of the given name"""
+        try:
+            value = nv.IMAQdxGetAttribute(self.imaqdx, name.encode('utf8'))
+            if isinstance(value, nv.core.IMAQdxEnumItem):
+                value = value.Name
+            if isinstance(value, bytes):
+                value = value.decode('utf8')
+            return value
+        except Exception as e:
+            # Add some info to the exception:
+            raise Exception(f"Failed to get attribute {name}") from e
+
+    def snap(self):
+        """Acquire a single image and return it"""
+        nv.IMAQdxSnap(self.imaqdx, self.img)
+        return self._decode_image_data(self.img)
+
+    def configure_acquisition(self, continuous=True, bufferCount=5):
+        nv.IMAQdxConfigureAcquisition(
+            self.imaqdx, continuous=continuous, bufferCount=bufferCount
+        )
+        nv.IMAQdxStartAcquisition(self.imaqdx)
+
+    def grab(self, waitForNextBuffer=True):
+        nv.IMAQdxGrab(self.imaqdx, self.img, waitForNextBuffer=waitForNextBuffer)
+        return self._decode_image_data(self.img)
+
+    def grab_multiple(self, n_images, images, waitForNextBuffer=True):
+        print(f"Attempting to grab {n_images} images.")
+        for i in range(n_images):
+            while True:
+                if self._abort_acquisition:
+                    print("Abort during acquisition.")
+                    self._abort_acquisition = False
+                    return
+                try:
+                    images.append(self.grab(waitForNextBuffer))
+                    print(f"Got image {i+1} of {n_images}.")
+                    break
+                except nv.ImaqDxError as e:
+                    if e.code == nv.IMAQdxErrorTimeout.value:
+                        print('.', end='')
+                        continue
+                    raise
+        print(f"Got {len(images)} of {n_images} images.")
+
+    def stop_acquisition(self):
+        nv.IMAQdxStopAcquisition(self.imaqdx)
+        nv.IMAQdxUnconfigureAcquisition(self.imaqdx)
+
+    def abort_acquisition(self):
+        self._abort_acquisition = True
+
+    def _decode_image_data(self, img):
+        img_array = nv.imaqImageToArray(img)
+        img_array_shape = (img_array[2], img_array[1])
+        # bitdepth in bytes
+        bitdepth = len(img_array[0]) // (img_array[1] * img_array[2])
+        dtype = {1: np.uint8, 2: np.uint16, 4: np.uint32}[bitdepth]
+        data = np.frombuffer(img_array[0], dtype=dtype).reshape(img_array_shape)
+        return data.copy()
+
+    def close(self):
+        nv.IMAQdxCloseCamera(self.imaqdx)
+
+
 # Demonstrate field propogation
 if __name__ == "__main__":
     import os
@@ -168,4 +318,30 @@ if __name__ == "__main__":
     
     pyplot.style.use(path + '/matplotlibrc')
     
-    # LightCrafterWorker()
+    # DMD =  LightCrafterWorker()
+    # Camera= IMAQdx_Camera(sn)
+    
+    pattern = np.zeros((HEIGHT, WIDTH))
+    pattern_bpm = arr_to_bmp(pattern)
+    
+    # DMD.program_manual(pattern_bpm)
+    
+    time.sleep(1)
+    # image = Camera.snap()
+    image = np.zeros((HEIGHT, WIDTH))
+    
+    # DMD.shutdown()
+    # Camera.close()
+
+    fig = pyplot.figure(figsize=(12,6))
+    gs = fig.add_gridspec(1, 2)
+    gs.update(left=0.13, right=0.95, top=0.92, bottom = 0.15, hspace=0.5, wspace = 0.35)  
+
+    ax = fig.add_subplot(gs[0,0])
+    ax.imshow(image)
+    
+    ax = fig.add_subplot(gs[0,1])
+    ax.imshow(pattern)
+
+    
+    
