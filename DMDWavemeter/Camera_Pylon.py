2d376bb97498ba7d378f28bec22e94855961b211
ispielma <ian.spielman@nist.gov>
2020-03-24 17:52:24 -0400

    Get parameter names now working in camera class.  Values will be easy.

--- DMDWavemeter/Camera_Pylon.py
+++ DMDWavemeter/Camera_Pylon.py
@@ -8,7 +8,7 @@ Created on Tue Mar 24 08:01:11 2020
 
 import numpy as np
 import Camera_Generic
-from pypylon import pylon
+from pypylon import pylon, genicam
 
 class Camera(Camera_Generic.Camera):
         
@@ -32,12 +32,86 @@ class Camera(Camera_Generic.Camera):
         print("Connecting to camera...")
         self.pylon_camera = pylon.InstantCamera(self.camera)
         self.pylon_camera.Open()
+        self._nodemap = self.pylon_camera.GetNodeMap()
         
         # Keep an img attribute so we don't have to create it every time
         # self._img = nv.imaqCreateImage(nv.IMAQ_IMAGE_U16)
 
     def close(self):
         self.pylon_camera.Close()
+
+    def get_attribute_names(self, visibility_string, writeable_only=True):
+        """Return a list of all attribute names of readable attributes, for the given
+        visibility level. Optionally return only writeable attributes
+        
+        This will be accomplished by recursivly traveling the nodemap
+        """
+        visibilities = {
+            'simple': 0,
+            'intermediate': 1,
+            'advanced': 2,
+        }
+        visibility_level = visibilities[visibility_string.lower()]
+    
+        inodes = self._nodemap.GetNodes()
+        
+        Names = []
+        for inode in inodes:
+            node = inode.GetNode()
+            
+            # now filter this guy
+            if not node.IsFeature():
+                continue
+    
+            if node.GetVisibility() > visibility_level:
+                continue
+            
+            if not hasattr(inode, 'GetValue'):
+                continue
+            
+            accessmode = genicam.EAccessModeClass_ToString(node.GetAccessMode())
+                
+            if writeable_only:
+                if accessmode.lower() not in ['rw'] or not hasattr(inode, 'SetValue') :
+                    continue
+            elif accessmode.lower() not in ['rw', 'ro']:
+                continue
+            
+            Names.append(self._GetFullNodeName(node))
+    
+        return sorted(Names)
+        
+    def _GetFullNodeName(self, node):
+            
+        parents = node.GetParents()
+            
+        Names = [node.GetName()]
+        while len(parents) > 0:
+            node = parents[0].GetNode()
+            parents = node.GetParents()
+            Name = node.GetName()
+                
+            # Avoid infinite recursion
+            if Name in Names:
+                break
+                
+            Names = [Name] + Names
+            
+        FullName = Names.pop()
+        Names.reverse()
+        for Name in Names:
+            FullName = Name + "::" + FullName
+            
+        return FullName
+    
+def GetValue(camera, key, nodemap=None):
+    if nodemap is None:
+        nodemap = camera.GetNodeMap()
+    inode = nodemap.GetNode(key)
+    
+    value = inode.GetValue()
+
+    return value
 
 # Demonstrate field propogation
 if __name__ == "__main__":
@@ -49,7 +123,7 @@ if __name__ == "__main__":
     pyplot.style.use(path + '/matplotlibrc')
 
     with Camera(0x14eef0d) as cam:
-        cam.camera.GetName()
-        pass
+        attrs = cam.get_attribute_names("intermediate", writeable_only=True)
+        print(attrs)
         
 
--- DMDWavemeter/pylon_test.py
+++ DMDWavemeter/pylon_test.py
@@ -11,7 +11,7 @@ from pypylon import pylon, genicam
 
 
 
-def RecurseNodeTree(Roots, visibility_string, ParentName='',  writeable_only=True):
+def RecurseNodeTree(camera, visibility_string, writeable_only=True):
     """
     Recurse  the Node tree and return a list of redreadable nodes
     writeable_only further limits to rw
@@ -24,59 +24,57 @@ def RecurseNodeTree(Roots, visibility_string, ParentName='',  writeable_only=Tru
     }
     visibility_level = visibilities[visibility_string.lower()]
 
-    
-    Names = []
-    for inode in Roots:
-        node = inode.GetNode()
-        if node.IsFeature():
-
-            visibility = node.GetVisibility()
-            accessmode = genicam.EAccessModeClass_ToString(node.GetAccessMode())
-            if len(ParentName) == 0:
-                FullName = node.GetName()
-            else:
-                FullName = ParentName + '::' + node.GetName()
-
-            children = node.GetChildren()
-            
-            # If we have some children, see if they are to be added
-            if len(children) > 0:
-                NewNames = RecurseNodeTree(children, visibility_string, ParentName=FullName)
-            else:
-                NewNames = []
-            
-            if len(NewNames) > 0:
-                Names += NewNames
-            
-            # Make sure we can set and get values
-            
-            
-            if visibility <= visibility_level:
-                if writeable_only:
-                    if accessmode.lower() in ['rw'] and hasattr(inode, 'GetValue') and hasattr(inode, 'SetValue') :
-                        Names.append(FullName)
-                elif accessmode.lower() in ['rw', 'ro'] and hasattr(inode, 'GetValue'):
-                    Names.append(FullName)
-
-    return Names
-        
-def GetRoots(camera):
     nodemap = camera.GetNodeMap()
     inodes = nodemap.GetNodes()
     
-    Roots = []
+    Names = []
     for inode in inodes:
         node = inode.GetNode()
-        children = node.GetChildren()
-        parents = node.GetParents()
-        feature = node.IsFeature()
-    
         
-        # Find Root features
-        if feature and len(parents) == 0 and len(children) > 0:
-            Roots.append(inode)
+        # now filter this guy
+        if not node.IsFeature():
+            continue
+
+        if node.GetVisibility() > visibility_level:
+            continue
+        
+        if not hasattr(inode, 'GetValue'):
+            continue
+        
+        accessmode = genicam.EAccessModeClass_ToString(node.GetAccessMode())
             
-    return Roots
+        if writeable_only:
+            if accessmode.lower() not in ['rw'] or not hasattr(inode, 'SetValue') :
+                continue
+        elif accessmode.lower() not in ['rw', 'ro']:
+            continue
+        
+        Names.append(GetFullName(node))
+
+    return Names
+        
+def GetFullName(node):
+    
+    parents = node.GetParents()
+    
+    Names = [node.GetName()]
+    while len(parents) > 0:
+        node = parents[0].GetNode()
+        parents = node.GetParents()
+        Name = node.GetName()
+        
+        # Avoid infinite recursion
+        if Name in Names:
+            break
+        
+        Names.append( Name )
+    Names.reverse()
+    
+    FullName = Names.pop()
+    for Name in Names:
+        FullName = Name + "::" + FullName
+    
+    return FullName
     
 def GetValue(camera, key, nodemap=None):
     if nodemap is None:
@@ -91,8 +89,6 @@ cam = pylon.TlFactory.GetInstance().CreateFirstDevice()
 camera = pylon.InstantCamera(cam)
 camera.Open()
 
-Roots = GetRoots(camera)
-Keys = RecurseNodeTree(Roots, 'intermediate')
-AllKeys = {k: GetValue(camera, k.split("::")[-1]) for k in Keys}
+Keys = RecurseNodeTree(camera, "advanced", writeable_only=True)
 
 camera.Close()
